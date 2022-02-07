7c1fabf8aea108a90c6eebbf0e0b9bbc0f119fdb
kungn103 <kungn103@newschool.edu>
2022-02-07 18:02:58 -0500

    simplifying

--- studio/projects/P1-platforms/secret-message/assets/main.js
+++ studio/projects/P1-platforms/secret-message/assets/main.js
@@ -4,7 +4,7 @@ function enterPassword() {
   let text;
   let person = window.prompt("WHAT'S THE PASSWORD?");
   if (person == "dead florals") {
-    text = "R0ZNdmx6U0JrTVBIYUVRTmFjNDhnTE9kUHhhclJ4RG5lbjF0djk0dENCbDBFcG1BMTVaQkQyZ0VpM25FWjdRNStheW5sSldNN2Z1T053SmUy\nRUpha2pjR25hWlFabElCQTBaTmlNTmc0VlVnV0xuVmNwd0lpTU1wNGVpaGx4OHV1MjVYU280cytiZ0hRaXpNVldIektHY0RyUk1PRUhyYlQ5LzRXZThwWkdtY\nUIxSkdCMnpNRXFGSFlIbDdKdlVuUlh2UjhnRW5GWW10ckNKYXdqeGtrK3RXallscUk3aXJQc3Y5bE9USjFsbEs4bEhCSEVKcDBmK3JWdUpsTmNnVEVZTnd\nVaEVDOUVHMFk3OTFhRHVkVnJudWN5RmlPSlN5eFJGU01LWjRTak9NSG1paGoyc2Naam95UUx\nVRUFlckhTZmhnQWtjMU56OTZENWNEY3NTREN2VzRBa0NQc2xqM2VrT1VRMnRGSGZ1Y3VkbWhT\ncDVxenloVDRONEpXTS93Ojoqtga3rTCKTIQ39UfaKrhg"
+    text = "SURNLzVrTWxiUEx3SlU2MlIrcUlIOGlxNEl2ZXNIZnhmdXNnbWcvWmoyeVJWcnV2Q3l4d2QyTXN2d0JqV1hHVkkyc1RPUE9RTGJ6Y2pvOHlha2xxYUhuZj\nNka0s1MGVpdlVtQzVWZVE5Vlk9OjpmDt+kr5g8BbXqFmCjReTy"
   } 
   else {
     text = "Refresh. Try Again."; 
